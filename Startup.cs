e6c97cadd8bda6529b9f586d62afb7ad21bab4cf
atik <mdatikhasan24434@gmail.com>
2022-04-21 00:43:43 +0600

    first commit

---
+++ Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(SignalR_Project.Startup))]
+
+namespace SignalR_Project
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
